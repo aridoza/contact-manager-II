10228300a855ac20f57016ab5b0677a17e1cdaae
aridoza <aridoza85@gmail.com>
2016-09-16 15:34:15 -0400

    mlab test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db;
 
 // Connect to the database before starting the application server
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGOLAB_URI, function (err, database) {
   if (err){
     console.log(err);
     process.exit(1);
