c4a00b15d8529431fa4f806fe9908fd7f21db416
aridoza <aridoza85@gmail.com>
2016-09-16 20:00:29 -0400

    testing full app

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   }
 
   app.get("/contacts", function(req, res) {
-    // db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-    //   if (err) {
-    //     handleError(res, err.message, "Failed to get contacts");
-    //   } else {
-    //     res.status(200).json(docs);
-    //   }
-    // });
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
   });
 
   app.post("/contacts", function(req, res) {
@@ -72,34 +72,34 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   //   DELETE: delete contact by id
 
   app.get("/contacts/:id", function(req, res) {
-    // db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-    //   if (err) {
-    //     handleError(res, err.message, "Failed to get contact");
-    //   } else {
-    //     res.status(200).json(doc);
-    //   }
-    // });
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contact");
+      } else {
+        res.status(200).json(doc);
+      }
+    });
   });
 
   app.put("/contacts/:id", function(req, res) {
-    // const updateDoc = req.body;
-    // delete updateDoc._id;
-    //
-    // db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    //   if (err) {
-    //     handleError(res, err.message, "Failed to update contact");
-    //   } else {
-    //     res.status(204).end();
-    //   }
-    // });
+    const updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update contact");
+      } else {
+        res.status(204).end();
+      }
+    });
   });
 
   app.delete("/contacts/:id", function(req, res) {
-    // db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    //   if (err) {
-    //     handleError(res, err.message, "Failes to delete contact");
-    //   } else {
-    //     res.status(204).end();
-    //   }
-    // });
+    db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failes to delete contact");
+      } else {
+        res.status(204).end();
+      }
+    });
   });
